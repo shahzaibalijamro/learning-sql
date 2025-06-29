cb39379922a2716c2e812c11e60abb168f2771ba
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-06-29 19:33:14 +0500

    section 2 challenges completed

--- 04-Challenge/index.js
+++ 04-Challenge/index.js
@@ -9,16 +9,10 @@
 // app.get("/", async (req,res)=>{
 //     const [customers] = await db.execute(`SELECT COUNT(*) FROM payments WHERE amount > 5`);
 //     console.log(customers);
-//     if (customers.length === 0) {
-//         res.send(
-//         "No customer found"
-//     )
-//     }else{
 //         res.send(
 //             `<h1>HERE IS THE COUNT OF PAYMENTS THAT ARE > 5$</h1>
 //             COUNT = ${customers[0]['COUNT(*)']}`
 //         )
-//     }
 // })
 
 // app.listen(3000, ()=>{
@@ -37,16 +31,10 @@
 // app.get("/", async (req,res)=>{
 //     const [customers] = await db.execute(`SELECT COUNT(*) FROM films WHERE film_name LIKE 'P%'`);
 //     console.log(customers);
-//     if (customers.length === 0) {
-//         res.send(
-//         "No customer found"
-//     )
-//     }else{
 //         res.send(
 //             `<h1>HERE IS THE COUNT OF FILM NAMES THAT START WITH THE LETTER P</h1>
 //             COUNT = ${customers[0]['COUNT(*)']}`
 //         )
-//     }
 // })
 
 // app.listen(3000, ()=>{
@@ -65,18 +53,57 @@
 // app.get("/", async (req,res)=>{
 //     const [customers] = await db.execute(`SELECT COUNT(DISTINCT(district)) FROM customers`);
 //     console.log(customers);
-//     if (customers.length === 0) {
-//         res.send(
-//         "No customer found"
-//     )
-//     }else{
 //         res.send(
 //             `<h1>HERE IS THE COUNT OF HOW MANY UNIQUE DISTRICTS ARE OUR CUSTOMERS FROM</h1>
 //             COUNT = ${customers[0]['COUNT(DISTINCT(district))']}`
 //         )
-//     }
 // })
 
 // app.listen(3000, ()=>{
 //     console.log("server is running");
 // });
+
+
+
+
+// ///// CHALLENGE 4: HOW MANY FILMS HAVE A RATING OF R AND A REPLACEMENT COST BW 5 AND 15?
+
+
+// import express from "express";
+// import { db } from "./db/db.js";
+
+// const app = express();
+
+// app.get("/", async (req, res) => {
+//     const [films] = await db.execute(`SELECT COUNT(*) FROM FILMS WHERE rating = 'R' AND replacement_cost BETWEEN 5 AND 15`);
+//     res.send(
+//         `<h1>HERE IS THE COUNT OF HOW MANY FILMS HAVE A RATING OF R AND A REPLACEMENT COST BW 5 AND 15</h1>
+//             COUNT = ${films[0]['COUNT(*)']}`
+//     )
+// })
+
+// app.listen(3000, () => {
+//     console.log("server is running");
+// });
+
+
+// ///// CHALLENGE 5: HOW MANY FILMS HAVE THE WORD Truman somewhere in the title?
+
+
+import express from "express";
+import { db } from "./db/db.js";
+
+const app = express();
+
+app.get("/", async (req,res)=>{
+    const [films] = await db.execute(`SELECT COUNT(*) as count FROM FILMS WHERE title LIKE '%Truman%'`);
+    console.log(films);
+        res.send(
+            `<h1>HERE IS THE COUNT OF HOW MANY FILMS HAVE THE WORD Truman somewhere in the title</h1>
+            COUNT = ${films[0].count}`
+        )
+})
+
+app.listen(3000, ()=>{
+    console.log("server is running");
+});
